1b2fb417746b3294ebbf0da94f4087edf93038e4
Tracy Falba <tfalba@mac.com>
2021-02-21 08:00:23 -0500

    border colors on feeds

--- src/components/TeamFeed.js
+++ src/components/TeamFeed.js
@@ -2,7 +2,7 @@ const TeamFeed = ({ team }) => {
   return (
     <>
       {team && (
-        <div style={{ backgroundColor: `${team.dashboard_style}` }} className='team-feed-container'>
+        <div style={{ border: `3px solid ${team.dashboard_style}`, backgroundColor: `${team.dashboard_style}` }} className='team-feed-container'>
           <h1>Feed will go here:</h1>
           <ul>
             <li>notifications</li>
