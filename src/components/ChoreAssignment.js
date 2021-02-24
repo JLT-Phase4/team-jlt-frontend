7bf503d2ef987fca749e230076c68e4aaceb052d
jesseyproctor <jss.proctor@gmail.com>
2021-02-23 21:09:39 -0500

    map through fake data

--- src/components/ChoreAssignment.js
+++ src/components/ChoreAssignment.js
@@ -1,39 +1,28 @@
 
+const fakeTeamMembers = [
+  { username: 'Logan', assignment_type: ['MD', 'TUE', 'WED', 'THUR', 'FRI', 'SAT', 'SUN'] },
+  { username: 'Tracy', assignment_type: ['MD', 'TUE', 'WED', 'THUR', 'FRI', 'SAT', 'SUN'] }
+]
+
+const fakeChores = [
+  { chore: ['groom_mustache', 'call_girlfriend', 'be_stoic', 'create_skynet', 'encourage_Jesse'] }
+]
+
 function ChoreAssignment () {
   return (
     <div className='page-container'>
       <div className='drag-and-drop-container'>
-        <div className='team-member-container'>
-          <div className='team-member'>name</div>
-          <div className='drag-and-drop-chore'>
-            <div>
-              <p>CHORE1</p>
-            </div>
+        {fakeTeamMembers.map((name, nameI) => (
+          <div key={name.username} className='team-member-container'>
+            <div className='team-member'>{name.username}</div>
+            {name.assignment_type.map((day, dayI) => (
+              <div draggable key={day} className='drag-and-drop-chore'>
+                {day}
+              </div>
+            ))}
           </div>
-          <div className='drag-and-drop-chore'>
-            <div>
-              <p>CHORE2</p>
-            </div>
-          </div>
-          <div className='drag-and-drop-chore'>
-            <div>
-              <p>CHORE3</p>
-            </div>
-          </div>
-        </div>
-        <div className='team-member-container'>
-          <div className='team-member'>name</div>
-          <div className='drag-and-drop-chore'>
-            <div>
-              <p>CHORE1</p>
-            </div>
-          </div>
-          <div className='drag-and-drop-chore'>
-            <div>
-              <p>CHORE2</p>
-            </div>
-          </div>
-        </div>
+        ))}
+
       </div>
     </div>
   )
