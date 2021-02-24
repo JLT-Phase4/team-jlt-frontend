903f4d11a7edae29734a224d8b506d4cba79ae69
jesseyproctor <jss.proctor@gmail.com>
2021-02-23 20:24:20 -0500

    getting started on dd

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ import laundryImage from './images/laundry-basket.png'
 import lawnMowingImage from './images/lawn-mowing.png'
 import walkingDogImage from './images/walking-dog.png'
 import washingDishesImage from './images/washing-dishes.png'
+import ChoreAssignment from './components/ChoreAssignment'
 
 const useUsername = createPersistedState('username')
 const useToken = createPersistedState('token')
@@ -118,6 +119,12 @@ function App () {
         <Route path='/choredetail/:chorePk'>
           <div className='App' />
           <ChoreRecordDetail token={token} />
+        </Route>
+
+        {/* CHORE ASSIGNMENT PAGE */}
+        <Route path='/chore-assignment/:teamPk'>
+          <div className='App' />
+          <ChoreAssignment token={token} />
         </Route>
 
         <Route path='/create-team-members/:teamPk'>
---
+++ src/components/ChoreAssignment.js
@@ -0,0 +1,42 @@
+
+function ChoreAssignment () {
+  return (
+    <div className='page-container'>
+      <div className='drag-and-drop-container'>
+        <div className='team-member-container'>
+          <div className='team-member'>name</div>
+          <div className='drag-and-drop-chore'>
+            <div>
+              <p>CHORE1</p>
+            </div>
+          </div>
+          <div className='drag-and-drop-chore'>
+            <div>
+              <p>CHORE2</p>
+            </div>
+          </div>
+          <div className='drag-and-drop-chore'>
+            <div>
+              <p>CHORE3</p>
+            </div>
+          </div>
+        </div>
+        <div className='team-member-container'>
+          <div className='team-member'>name</div>
+          <div className='drag-and-drop-chore'>
+            <div>
+              <p>CHORE1</p>
+            </div>
+          </div>
+          <div className='drag-and-drop-chore'>
+            <div>
+              <p>CHORE2</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ChoreAssignment
