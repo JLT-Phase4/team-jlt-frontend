aaba2aecaee6c8f86a99f7024dd60fed72679d91
jesseyproctor <jss.proctor@gmail.com>
2021-02-24 00:11:12 -0500

    fix background affect, drag but still no drop

--- src/components/ChoreAssignment.js
+++ src/components/ChoreAssignment.js
@@ -1,17 +1,48 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 const data = [
-  { assignment_type: 'MON', chores: ['groom_mustache', 'call_girlfriend', 'be_stoic'] },
-  { assignment_type: 'TUE', chores: ['create_skynet', 'encourage_Jesse'] },
-  { assignment_type: 'WED', chores: ['create_skynet', 'encourage_Jesse'] },
-  { assignment_type: 'THUR', chores: ['reinvent wheel', 'brush_teeth'] },
-  { assignment_type: 'FRI', chores: ['end_skynet'] },
-  { assignment_type: 'SAT', chores: ['wash_car'] },
-  { assignment_type: 'SUN', chores: ['make_dollars'] }
+  { assignment_type: 'MON', chores: ['groom mustache', 'call girlfriend', 'be stoic'] },
+  { assignment_type: 'TUE', chores: ['create skynet', 'encourage Jesse'] },
+  { assignment_type: 'WED', chores: ['create skynet', 'encourage Jesse'] },
+  { assignment_type: 'THUR', chores: ['reinvent wheel', 'brush teeth'] },
+  { assignment_type: 'FRI', chores: ['end skynet'] },
+  { assignment_type: 'SAT', chores: ['wash car'] },
+  { assignment_type: 'SUN', chores: ['make dollars'] }
 ]
 const names = ['Logan', 'Tracy', 'Jesse']
 
 function ChoreAssignment () {
+  const [list, setList] = useState(data)
+  const [dragging, setDragging] = useState(false)
+  const dragItem = useRef()
+  const dragNode = useRef()
+
+  const handleDragStart = (event, parameters) => {
+    // console.log(parameters)
+    dragItem.current = parameters
+    dragNode.current = event.target
+    dragNode.current.addEventListener('dragend', handleDragEnd)
+    setTimeout(() => {
+      setDragging(true)
+    }, 0)
+  }
+
+  const handleDragEnd = () => {
+    console.log('its working!')
+    setDragging(false)
+    dragNode.current.removeEventListener('dragend', handleDragEnd)
+    dragItem.current = null
+    dragNode.current = null
+  }
+
+  const getStyles = (parameters) => {
+    const currentItem = dragItem.current
+    if (currentItem.itemI === parameters.itemI && currentItem.choreI === parameters.choreI) {
+      return 'current drag-and-drop-chore'
+    }
+    return 'drag-and-drop-chore'
+  }
+
   return (
     <div>
       <div>
@@ -20,11 +51,15 @@ function ChoreAssignment () {
             <div className='team-member'>{name}</div>
 
             <div className='drag-and-drop-container'>
-              {data.map((item, itemI) => (
+              {list.map((item, itemI) => (
                 <div key={item.assignment_type} className='day-container'>
                   <div className='days'>{item.assignment_type}</div>
                   {item.chores.map((chore, choreI) => (
-                    <div draggable key={chore} className='drag-and-drop-chore'>
+                    <div
+                      draggable onDragStart={(event) => { handleDragStart(event, { itemI, choreI }) }}
+                      key={chore}
+                      className={dragging ? getStyles({ itemI, choreI }) : 'drag-and-drop-chore'}
+                    >
                       {chore}
                     </div>
                   ))}
