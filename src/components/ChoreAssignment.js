6cc246d587e971ca889e01e128210fcdefdfad0b
jesseyproctor <jss.proctor@gmail.com>
2021-02-23 22:34:36 -0500

    nested mapping correct, layout inccorect. save now or you may lose your mind

--- src/components/ChoreAssignment.js
+++ src/components/ChoreAssignment.js
@@ -1,28 +1,39 @@
+import { useState } from 'react'
 
-const fakeTeamMembers = [
-  { username: 'Logan', assignment_type: ['MD', 'TUE', 'WED', 'THUR', 'FRI', 'SAT', 'SUN'] },
-  { username: 'Tracy', assignment_type: ['MD', 'TUE', 'WED', 'THUR', 'FRI', 'SAT', 'SUN'] }
+const data = [
+  { assignment_type: 'MON', chores: ['groom_mustache', 'call_girlfriend', 'be_stoic'] },
+  { assignment_type: 'TUE', chores: ['create_skynet', 'encourage_Jesse'] },
+  { assignment_type: 'WED', chores: ['create_skynet', 'encourage_Jesse'] },
+  { assignment_type: 'THUR', chores: ['reinvent wheel', 'brush_teeth'] },
+  { assignment_type: 'FRI', chores: ['end_skynet'] },
+  { assignment_type: 'SAT', chores: ['wash_car'] },
+  { assignment_type: 'SUN', chores: ['make_dollars'] }
 ]
-
-const fakeChores = [
-  { chore: ['groom_mustache', 'call_girlfriend', 'be_stoic', 'create_skynet', 'encourage_Jesse'] }
-]
+const names = ['Logan', 'Tracy', 'Jesse']
 
 function ChoreAssignment () {
   return (
-    <div className='page-container'>
-      <div className='drag-and-drop-container'>
-        {fakeTeamMembers.map((name, nameI) => (
-          <div key={name.username} className='team-member-container'>
-            <div className='team-member'>{name.username}</div>
-            {name.assignment_type.map((day, dayI) => (
-              <div draggable key={day} className='drag-and-drop-chore'>
-                {day}
-              </div>
-            ))}
+    <div className='drag-and-drop-container'>
+      <div>
+        {names.map((name) => (
+          <div key={name} className='team-member-container'>
+            <div className='team-member'>{name}</div>
+
+            <div>
+              {data.map((item, itemI) => (
+                <div key={item.assignment_type} className='day-container'>
+                  <div className='days'>{item.assignment_type}</div>
+                  {item.chores.map((chore, choreI) => (
+                    <div draggable key={chore} className='drag-and-drop-chore'>
+                      {chore}
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+
           </div>
         ))}
-
       </div>
     </div>
   )
