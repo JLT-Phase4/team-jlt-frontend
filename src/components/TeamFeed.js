d2d16882448bada6b0639268dfac5763477275d3
jesseyproctor <jss.proctor@gmail.com>
2021-03-02 10:51:20 -0500

    pass logged in user to feed comp

--- src/components/TeamFeed.js
+++ src/components/TeamFeed.js
@@ -1,47 +1,48 @@
-// import { useState, useEffect } from 'react'
-// import { postMessage, getFeed } from './../api'
+import { useState, useEffect } from 'react'
+import { postMessage, getFeed } from './../api'
 
-// function TeamFeed ({ teamPk }) {
-//   const [message, setMessage] = useState('')
-//   const [feed, setFeed] = useState()
+function TeamFeed ({ token, teamPk, profileUsername }) {
+  const [message, setMessage] = useState('')
+  const [feed, setFeed] = useState()
+  const author = profileUsername
 
-//   useEffect(renderFeed, [token, teamPk])
+  useEffect(renderFeed, [token, teamPk])
 
-//   function renderFeed () {
-//     event.preventDefault()
-//     getFeed(token, teamPk)
-//       .then(feed => setFeed(feed))
-//   }
+  function renderFeed (event) {
+    event.preventDefault()
+    getFeed(token, teamPk)
+      .then(feed => setFeed(feed))
+  }
 
-//   //   function handleAddMessage (event) {
-//   //     event.preventDefault()
-//   //     postMessage(token, teamPk, author, message)
-//   //       .then(data => updateFeed(data.message))
-//   //   }
+  function handleAddMessage (event) {
+    event.preventDefault()
+    postMessage(token, teamPk, author, message)
+      .then(data => updateFeed(data.message))
+  }
 
-//   //   function updateFeed() {
-//   //     // setMessageList by pushing new message onto end of array
-//   //   }
+  function updateFeed () {
+    // setMessageList by pushing new message onto end of array
+  }
 
-//   return (
-//     <div>
-//       <div className='flex'>
-//         {feed.notifications.map(notification => (
-//           <ul key={notification}>
-//             <li>
-//               {notification}
-//             </li>
-//           </ul>
-//         ))}
-//       </div>
+  return (
+    <div>
+      <div className='flex'>
+        {feed.notifications.map(notification => (
+          <ul key={notification}>
+            <li>
+              {notification}
+            </li>
+          </ul>
+        ))}
+      </div>
 
-//       <div>
-//         <form className='add-message' onSubmit={handleAddMessage}>
-//           <input type='text' placeholder='Write a comment' value={message} onChange={event => setMessage(event.target.value)} />
-//           <button type='submit'>Post</button>
-//         </form>
-//       </div>
-//     </div>
-//   )
-// }
-// export default TeamFeed
+      <div>
+        <form className='add-message' onSubmit={handleAddMessage}>
+          <input type='text' placeholder='Write a comment' value={message} onChange={event => setMessage(event.target.value)} />
+          <button type='submit'>Post</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+export default TeamFeed
