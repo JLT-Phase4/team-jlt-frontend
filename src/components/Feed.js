a55b088edbf7bff9c7765518cad4dee021909136
jesseyproctor <jss.proctor@gmail.com>
2021-03-04 13:21:16 -0500

    avitar showing

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -4,6 +4,7 @@ import { postMessage, getFeed } from '../api'
 function Feed ({ token, profileUsername, today, feedPk }) {
   const [message, setMessage] = useState()
   const [feed, setFeed] = useState()
+  const AVATAR = 'https://images.unsplash.com/photo-1563396983906-b3795482a59a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMDg5MDF8MHwxfHNlYXJjaHw5fHxyb2JvdHxlbnwwfDB8fA&ixlib=rb-1.2.1&q=80&w=1080'
 
   function handleSubmit (event) {
     event.preventDefault()
@@ -20,27 +21,26 @@ function Feed ({ token, profileUsername, today, feedPk }) {
   }
 
   return (
-    <div className='messages'>
+    <div>
       {feed && (
         <div>
           <div>
             <div>
               {feed.notifications.map(notification => (
-
-                <div className='message-content' key={notification.message + notification.target + today}>
-
-
+                <div key={notification.message + notification.target + today}>
+                  {/* <img src={notification.sender.avatar} /> */}
+                  <div className='avatar-holder' style={(notification.sender.avatar === undefined || notification.sender.avatar === '' || notification.sender.avatar === null) ? { backgroundImage: `url(${AVATAR})` } : { backgroundImage: `url(${notification.sender.avatar})` }} />
                   <p> {notification.sender.username}</p>
-                  <p>{notification.message}</p>
+                  <p className='message-container'>{notification.message}</p>
                 </div>
               ))}
             </div>
 
           </div>
           <div>
-            <form className='message-box' onSubmit={handleSubmit}>
-              <input className='message-input' type='text' placeholder='Write a comment...' value={message} onChange={event => setMessage(event.target.value)} />
-              <button className='message-submit' type='submit'>Send</button>
+            <form className='comment-box' onSubmit={handleSubmit}>
+              <input className='comment-input' type='text' placeholder='Write a comment...' value={message} onChange={event => setMessage(event.target.value)} />
+              <button className='comment-submit' type='submit'>Send</button>
             </form>
           </div>
         </div>
