4effc1709fd29e149091ffd3aa92a118c7183cbe
jesseyproctor <jss.proctor@gmail.com>
2021-03-09 17:14:00 -0500

    remove underline on nav bar links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -28,22 +28,22 @@ const Navigation = ({ token, myTeam, isCaptain, username, handleTime, handleLogo
     <div className='chore-wars-nav'>
       <div style={{ justifyContent: 'space-between' }} className='flex chore-wars-bar'>
         <div className='flex'>
-          <Link to='/' className='banner'>Chore Wars</Link>
+          <Link style={{ textDecoration: 'none' }} to='/' className='banner'>Chore Wars</Link>
         </div>
         {token && (
           <ul style={{ paddingTop: '10px' }} className='flex'>
-            <li className=''><Link to='/'>Pod</Link></li>
-            <li className=''><Link to={`/team/${myTeam}`}>Team</Link></li>
+            <li className=''><Link style={{ textDecoration: 'none' }} to='/'>Pod</Link></li>
+            <li className=''><Link style={{ textDecoration: 'none' }} to={`/team/${myTeam}`}>Team</Link></li>
             {isCaptain === true &&
-              <li className=''><Link to={`/chore-assignment/${myTeam}/`}>Assign Chores</Link></li>}
+              <li className=''><Link style={{ textDecoration: 'none' }} to={`/chore-assignment/${myTeam}/`}>Assign Chores</Link></li>}
             {isCaptain === false &&
-              <li className=''><Link to={`/user-profile/${username}`}>Profile</Link></li>}
-            <li className=''><Link to={`/team-chores/${myTeam}`}>{isCaptain ? 'Manage Chores' : 'Chores'}</Link></li>
+              <li className=''><Link style={{ textDecoration: 'none' }} to={`/user-profile/${username}`}>Profile</Link></li>}
+            <li className=''><Link style={{ textDecoration: 'none' }} to={`/team-chores/${myTeam}`}>{isCaptain ? 'Manage Chores' : 'Chores'}</Link></li>
           </ul>
         )}
 
         <div className='flex-row' style={{ paddingTop: '10px' }}>
-          <div className='about-link'><Link to='/about'>About</Link></div>
+          <div className='about-link'><Link style={{ textDecoration: 'none' }} to='/about'>About</Link></div>
           <div className='avatar-image-nav' style={{ backgroundImage: `url(${avatar})` }} />
           {isLoggedIn
             ? (
@@ -53,7 +53,7 @@ const Navigation = ({ token, myTeam, isCaptain, username, handleTime, handleLogo
               )
             : (
               <span>
-                <Link className='login-link' to='/login'>Login</Link>or<Link className='nav-bar-link' to='/register'>Register</Link>
+                <Link style={{ textDecoration: 'none' }} className='login-link' to='/login'>Login</Link>or<Link className='nav-bar-link' to='/register'>Register</Link>
               </span>
               )}
         </div>
